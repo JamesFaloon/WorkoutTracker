5c1e0fe6c498a517c599a14cbd3aa27342b83f25
James Faloon <james.faloon@bell.net>
2024-07-31 18:32:19 -0400

    Created all classes plus design toString and constructor for workoutplan class

---
+++ src/WorkoutTracker/driver.java
@@ -0,0 +1,10 @@
+package src.WorkoutTracker;
+
+public class driver {
+    public static void main(String[] args) {
+        WorkoutPlan p1 = new WorkoutPlan();
+
+        System.out.println(p1.toString());
+    }
+
+}
---
+++ src/WorkoutTracker/workout.java
@@ -0,0 +1,11 @@
+package src.WorkoutTracker;
+
+public class workout extends WorkoutPlan {
+
+    protected String name;
+
+    workout() {
+        this.name = "";
+    }
+
+}
